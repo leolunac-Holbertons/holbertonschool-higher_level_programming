aeb89f3144bd5908243dbabf1c501201a4bde0f7
leolunac-holbertons <2176@leolunac-Holbertons>
2020-12-08 18:13:54 +0000

    0x01. Python - if/else, loops, functions

--- 0x01-python-if_else_loops_functions/lists.h
+++ 0x01-python-if_else_loops_functions/lists.h
@@ -19,6 +19,7 @@ size_t print_listint(const listint_t *h);
 listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
 
+listint_t *create_node(int number);
 listint_t *insert_node(listint_t **head, int number);
 
 #endif /* LISTS_H */
