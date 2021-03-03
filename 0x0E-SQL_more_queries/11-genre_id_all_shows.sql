e14733dd66b8594c14f144a9dc5b0f7b98b7c39d
leolunac-holbertons <2176@leolunac-Holbertons>
2021-03-03 00:11:42 +0000

    Write a script that lists all shows contained in the database hbtn_0d_tvshows.

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,4 +1,6 @@
 --imports the database dump list all shows with title and genre id
+
+
 SELECT tv_shows.title AS title, tv_show_genres.genre_id AS genre_id
 FROM tv_shows
 LEFT OUTER JOIN tv_show_genres
