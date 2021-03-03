0279f998d02925c8caeb141b553f673e704b449c
leolunac-holbertons <2176@leolunac-Holbertons>
2021-03-03 00:06:13 +0000

    Import the database dump from hbtn_0d_tvshows to your MySQL server: download

---
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,7 @@
+-- import all shows that have one genre linked at least
+
+SELECT tv_shows.title AS title, tv_show_genres.genre_id AS genre_id
+FROM tv_shows
+INNER JOIN tv_show_genres
+ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC
